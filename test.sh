e523e3ad3d94d5ee16f290fbb3c769bd746b6214
Hubert Naguszewski <hubert.naguszewski@warwick.ac.uk>
2023-05-22 16:06:40 +0100

    Deleted unnecessary file

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gcc calculate_cluster.c functions/* -o bin/calculate_cluster
-(cd bin; ./calculate_cluster)
+#gcc calculate_cluster.c functions/* -o bin/calculate_cluster
+#(cd bin; ./calculate_cluster)
 gcc create_cluster_set_v2.c functions/* -o bin/test -g
-(cd bin; ./test 1 10000 1)
+(cd bin; ./test 1 10000 1 10)
