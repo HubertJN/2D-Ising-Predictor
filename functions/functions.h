f4d21f48f0d4f0121ded2e7ac2796e6bb2881b90
Hubert Naguszewski <hubert.naguszewski@warwick.ac.uk>
2023-06-13 14:12:46 +0100

    no longer necessary

--- functions/functions.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void read_input_grid(FILE *ptr, char *bitgrid, int L, int *ising_grids, int grids_per_slice, int islice, int igrid);
-int find_cluster_size(int L, int Maxcon, int *grid, int* Lcon, int* Ncon);
-int find_clusters_recursive(int Nvert, int Maxcon, int *Ncon, int *Lcon);
-int find_clusters_eqclass(int Nvert, int Maxcon, int *Ncon, int *Lcon);
-void vertex_search(int i, int icluster, int Maxcon, int *Ncon, int *Lcon, int *lvisited, int *cluster_size);
-void read_input_variables(int *L, int *nreplicas, int *nsweeps, int *mag_output_int, int *grid_output_int, int *threadsPerBlock, int *gpu_device, int *gpu_method, double *beta, double *h);
