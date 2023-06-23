7e0b5ebcb5e8c23cb562661606b09f6bf162c289
Hubert Naguszewski <hubert.naguszewski@warwick.ac.uk>
2023-06-23 15:44:05 +0100

    Changed certain variables

--- test.sh
+++ test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#gcc calculate_cluster.c functions/* -o bin/calculate_cluster
-#(cd bin; ./calculate_cluster)
-gcc create_cluster_set_v2.c functions/* -o bin/test -g
-(cd bin; ./test 1 10000 1 10)
+gcc create_cluster_set.c functions/read_input_variables.* functions/comparison.* -o bin/create_cluster_set -Wall -g
+#(cd bin; valgrind ./create_cluster_set 1000)
+(cd bin; ./create_cluster_set 1000)
