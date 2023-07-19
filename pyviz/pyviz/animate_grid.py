ec12cc592b839f2666c550fb4c03bd9254962079
Pip Grylls <pip.grylls@warwick.ac.uk>
2023-07-19 16:36:07 +0100

    visulisation mostly working downloads a large html file

--- pyviz/pyviz/animate_grid.py
+++ pyviz/pyviz/animate_grid.py
@@ -11,30 +11,43 @@ test_sim.load_all_grids()
 test_sim.convert_to_image_array()
 
 
-frames = [
-    go.Frame(
-        data=go.Image(z=test_sim.image_grids[i,0,:,:]), 
-        layout=go.Layout(
-            title_text=f"frame {i}",
-            width=600, height=600,
-        )
-    )
-    for i in range(test_sim.image_grids.shape[0])
-    ]
-data = frames[0].data
-layout=go.Layout(
-    width=600, height=600,
-    title="Ising Model",
-    updatemenus=[dict(
-        type="buttons",
-        buttons=[
-            dict(
-            label="Play",
-            method="animate",
-            args=[None]
-        )]
-    )]
-)
+test_sim.plot_frames()
+test_sim.make_figure()
+test_sim.create_layout()
+test_sim.animate_all_grids()
 
-fig = go.Figure(data=data, layout=layout, frames=frames)
-fig.show()
+
+
+# frames = [
+#     go.Frame(
+#         data=go.Image(z=test_sim.image_grids[i,0,:,:]), 
+#         layout=go.Layout(
+#             title_text=f"Copy 1, frame {i}, mag {test_sim.all_mag_nuc[i][0][0]}, nuc {test_sim.all_mag_nuc[i][0][1]}",
+#             width=600, height=600,
+#         )
+#     )
+#     for i in range(test_sim.image_grids.shape[0])
+#     ]
+# data = frames[0].data
+# layout=go.Layout(
+#     width=600, height=600,
+#     title="Ising Model",
+#     updatemenus=[dict(
+#         type="buttons",
+#         buttons=[
+#             dict(
+#             label="Play",
+#             method="animate",
+#             args=[None]
+#         )]
+#     )]
+# )
+
+# layout=go.Layout(
+#     title_text=[f"Copy {j}, frame {i}, mag {self.all_mag_nuc[i][j][0]}, nuc {self.all_mag_nuc[i][j][1]}" for j in range(self.config['num_concurrent'])],
+#     width=600, height=600,
+# )
+
+
+# fig = go.Figure(data=data, layout=layout, frames=frames)
+# fig.show()
