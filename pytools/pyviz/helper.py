bb3255938ca0e7d0e8af3761ce9582ffe43f63ec
Pip Grylls <pip.grylls@warwick.ac.uk>
2023-08-02 16:08:25 +0100

    testing putting in pygame

--- pytools/pyviz/helper.py
+++ pytools/pyviz/helper.py
@@ -1,9 +1,14 @@
+import struct
 import numpy as np
 from pathlib import Path
+import pygame
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from plotly_gif import GIF, capture
+import logging
+logging.basicConfig(filename='pyviz.log', encoding='utf-8', level=logging.DEBUG)
+
 
 class SimulationSet():
 
@@ -140,113 +145,166 @@ class Type1(Simulation):
     def load_all_grids(self):
         grids_and_mags = [self.load_grid_single(i) for i in self.file_names]
         self.all_grids = np.array([i[0] for i in grids_and_mags]).astype(int)
-        self.all_mag_nuc = np.array([i[1] for i in grids_and_mags]).astype(float)
+        self.all_mag = np.array([i[1] for i in grids_and_mags]).astype(float)
+        self.all_nuc = np.array([i[2] for i in grids_and_mags]).astype(int)
             
     def load_grid_set(self):
         pass
 
     def load_grid_single(self, file_name):
-        with open(file_name, 'r') as f:
-            lines = f.readlines()
-            get_dims = lines.pop(0)
-            dims = get_dims.split()[2:]
-            dims = [int(i) for i in dims]
-            grids = []
-            mag_nuc = []
-            for i in range(self.config['num_concurrent']):
-                # Copy 1, Mag -0.989800, Nucleated 0
-                copyline = lines.pop(0).split()
-                mag = float(copyline[3].strip(','))
-                nuc = int(copyline[5].strip(','))
-                assert (copyline[0], copyline[1].strip(',')) == ("Copy", f"{i+1}"), "File not formatted correctly"
-                grids.append(
-                    [lines.pop(0).split()
-                    for _ in range(dims[0])]
-                )
-                mag_nuc.append((mag, nuc))
-                lines.pop(0)
-        return grids, mag_nuc
+        endian = 'little' # ToDo use check to verify this...
+        sz_sz = 8
+        bytes_read = 0
+
+        file = open(file_name, "rb")
+
+        fst = file.read(sz_sz) # Size of magnetization data
+        bytes_read += sz_sz
+        mag_sz = int.from_bytes(fst, endian)
+        logging.info("Size of mag data (float):", mag_sz)
+
+        nxt = file.read(sz_sz) # Size of grid data
+        bytes_read += sz_sz
+        grid_sz = int.from_bytes(nxt, endian)
+        logging.info("Size of grid data (int):", grid_sz)
+
+        # Format for float unpacking
+        fmt = 'f'
+        if mag_sz == 8:
+            fmt = 'd'
+
+        check_raw = file.read(mag_sz)
+        bytes_read += mag_sz
+        check = struct.unpack(fmt, check_raw)[0]
+        print("Check value", check)
+
+        if check != 3.0/32.0: print("ERRROROR")
+
+        file_data=[]# Keep track of the jump offsets, for debugging
+
+        next_loc = int.from_bytes(file.read(sz_sz), endian)
+        bytes_read += sz_sz
+        file_data.append(next_loc)
+        logging.debug('#',next_loc)
+
+        n_dims = int.from_bytes(file.read(sz_sz), endian)
+        bytes_read += sz_sz
+        logging.debug("n_dims", n_dims)
+
+        dims = []
+        for i in range(n_dims):
+            dims.append(int.from_bytes(file.read(sz_sz), endian))
+            bytes_read += sz_sz
+
+        logging.info("dims", dims)
+
+        n_conc = int.from_bytes(file.read(sz_sz), endian)
+        logging.info("n_concurrent", n_conc)
+        bytes_read += sz_sz
+
+        next_loc = int.from_bytes(file.read(sz_sz), endian)
+        file_data.append(next_loc)
+        logging.debug("#", next_loc)
+        bytes_read += sz_sz
+
+        # Now we get all of the grids meta-data
+        for i in range(min(n_conc, 100)):  # Min while developing reader - prevent giant loop if n_conc is misread
+            indx = int.from_bytes(file.read(sz_sz), endian)
+
+            mag_raw = file.read(mag_sz)
+            mag = struct.unpack(fmt, mag_raw)[0]
+            nuc = int.from_bytes(file.read(sz_sz), endian)
+            logging.info("grid num:{}, magnetisation:{:.6f}, (nucleated?:{})".format(indx, mag, nuc))
+            bytes_read += sz_sz + mag_sz + sz_sz
+
+
+        total_sz = dims[0]
+        for i in range(1, len(dims)): total_sz *= dims[i]
+        logging.info("Data per grid:", total_sz)
+
+        grid_fmt_string = "i{}".format(grid_sz)
+        dt = np.dtype(grid_fmt_string)
+
+        # Now the actual grids
+        for i in range(min(n_conc, 100)):  # Min while developing reader - prevent giant loop if n_conc is misread
+            data = np.zeros(total_sz)
+            next_loc = int.from_bytes(file.read(sz_sz), endian)
+            file_data.append(next_loc)
+            raw_data = file.read(total_sz * grid_sz)
+            bytes_read += sz_sz + total_sz * grid_sz
+            data = np.frombuffer(raw_data, dt, count=total_sz)
+            data = np.reshape(data, dims)
+            logging.debug("Calculated magnetization: {}:".format(np.sum(data)))
+
+        
+        return data, mag, nuc
 
     def make_figure(self):
-        self.maxcols = 4
-        cols = min(self.config['num_concurrent'], self.maxcols)
-        rows = int(np.ceil(self.config['num_concurrent']/cols))
-
-        self.figure = make_subplots(rows=rows, cols=cols, 
-                                    subplot_titles=[f"Copy {i+1}" for i in range(self.config['num_concurrent'])], 
-                                    horizontal_spacing=0.1, vertical_spacing=0.15)
-
-        for i in range(self.config['num_concurrent']):
-            self.figure.add_trace(
-                go.Image(z=self.image_grids[i,0,:,:]),
-                row=(i//self.maxcols)+1, col=(i%self.maxcols)+1
-            )
+        self.create_layout()
+        pygame.init()
+        size = (self.block_size*self.config.size_x, self.block_size*self.config.size_y)
+        self.screen = pygame.display.set_mode(size)
+        pygame.display.set_caption(f"Sweep : {0:d}")
+        self.screen.fill(self.BLACK)
+        pygame.display.flip()
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.advance = True
     
     def create_layout(self):
-        # The buttons dont work either
-        # buttons = [
-        #     dict(
-        #         label='Play',
-        #         method='animate',
-        #         args=[
-        #             None, 
-        #             dict(
-        #                 frame=dict(duration=50, redraw=False), 
-        #                 transition=dict(duration=0),
-        #                 fromcurrent=True,
-        #                 mode='immediate'
-        #             )
-        #         ]
-        #     ),
-        #     dict(
-        #         label='Pause',
-        #         method='animate',
-        #         args=[
-        #             [None],
-        #             dict(
-        #                 frame=dict(duration=0, redraw=False), 
-        #                 transition=dict(duration=0),
-        #                 fromcurrent=True,
-        #                 mode='immediate'
-        #             )
-        #         ]
-        #     )
-        # ]
-
-
-        # Adding a slider
-        # This does not work as expected
-        # sliders = [{
-        #     'yanchor': 'top',
-        #     'xanchor': 'left', 
-        #     'active': 1,
-        #     'currentvalue': {'font': {'size': 16}, 'prefix': 'Steps: ', 'visible': True, 'xanchor': 'right'},
-        #     'transition': {'duration': 200, 'easing': 'linear'},
-        #     'pad': {'b': 10, 't': 50}, 
-        #     'len': 0.9, 'x': 0.15, 'y': 0, 
-        #     'steps': [{'args': [[k], {'frame': {'duration': 200, 'easing': 'linear', 'redraw': False},
-        #                               'transition': {'duration': 0, 'easing': 'linear'}}], 
-        #                               'label': k, 'method': 'animate'} for k in range(len(self.figure.frames) - 1)       
-        #         ]
-        # }]
-
-
-        self.figure.update_layout(
-            # updatemenus=[
-            #     dict(
-            #         type='buttons',
-            #         showactive=False,
-            #         y=0,
-            #         x=1.05,
-            #         xanchor='left',
-            #         yanchor='bottom',
-            #         buttons=buttons )
-            # ],
-            width=800, height=500
-        )
+        self.BLACK = (0, 0, 0)
+        self.WHITE = (255, 255, 255)
+        self.block_size = 10
+        self.color_map = [self.BLACK, self.WHITE]
+        self.igrid = 0
+        self.isweep = 0
+        
     
     def animate_all_grids(self):
-        self.figure.write_html(self.path_base/"animation_test.html")
+        while True:
+            irow_max = self.config.size_x
+            icol_max = self.config.size_y
+            npix = irow_max * icol_max
+            for ix in range(npix):
+                irow = ix // icol_max
+                icol = ix % icol_max
+                pygame.draw.rect(self.screen, self.color_map[self.all_grids[self.igrid][self.isweep][irow][icol]], 
+                                 (icol*self.block_size, irow*self.block_size, self.block_size, self.block_size))
+            self.process_events()
+            # Update and limit frame rate
+            time_string = "Grid : %d, Sweep : %.d" % (self.igrid, self.isweep)
+            pygame.display.set_caption(time_string)
+            pygame.display.flip()
+            self.clock.tick()
+            if not self.running:
+                break
+            if (self.advance):
+                self.isweep += 1
+            
+    def process_events(self):
+            # Process events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_p:
+                    advance = not advance
+                if event.key == pygame.K_LEFT:
+                    iframe = max(iframe-1,0)
+                    advance = False
+                if event.key == pygame.K_RIGHT:
+                    iframe += 1
+                    advance = False
+                if event.key == pygame.K_UP:
+                    igrid = min(self.igrid+1,self.config.num_concurrent-1)
+                if event.key == pygame.K_DOWN:
+                    igrid = max(igrid-1,0)
+                if event.key == pygame.K_w:
+                    outfile = open(f"{args.output_dir}/gridinput.bin","wb")
+                    outfile.write(L.tobytes())
+                    gridbuffer.tofile(outfile)
+                    outfile.close()
+                    print(f"Grid snapshot written to {args.output_dir}/gridinput.bin")
 
     def animate_grid_set(self):
         pass
