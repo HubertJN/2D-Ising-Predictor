db4e28b340ea20731021648688a601e1fb7413b3
Hubert Naguszewski <hubert.naguszewski@warwick.ac.uk>
2023-08-16 10:10:58 +0100

    Added some code to 'fix' data, i.e. exclude problematic data

--- NN/data_process.py
+++ NN/data_process.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import torch
 from torch.utils.data import Dataset
+import matplotlib.pyplot as plt
 
 gridstates = open('/home/eng/phunsc/PhD_Project/2D_Ising_Project/bin/gridstates.bin', 'rb')
 index_file = open('/home/eng/phunsc/PhD_Project/2D_Ising_Project/bin/committor_index.bin', 'rb')
@@ -10,8 +11,9 @@ byte_prefix = 4*(3)
 ngrids = 8192
 L = 64
 bytes_per_slice = byte_prefix+ngrids*(L*L/8)
-grid_data = np.zeros((1000,64*64), dtype=np.float32)
-committor_data = np.zeros((1000, 1), dtype=np.float32)
+grid_data = np.zeros((5000,64*64), dtype=np.float32)
+committor_data = np.zeros((5000, 2), dtype=np.float32)
+cluster = np.zeros((5000, 1), dtype=np.float32)
 i = 0
 ibit=0
 ibyte=0
@@ -35,22 +37,34 @@ while(1):
             if (isite>L*L):
                 break
     grid_data[i] = ising_grids
-    committor_data[i] = committor
+    committor_data[i, 0] = committor
+    committor_data[i, 1] = std_dev
+    cluster[i] = index[2]
     i += 1
-
+print(i)
 try:
     os.remove('/home/eng/phunsc/PhD_Project/2D_Ising_Project/NN/grid_data')
     os.remove('/home/eng/phunsc/PhD_Project/2D_Ising_Project/NN/committor_data')
 except:
     pass
+
+# fix data
+#cluster = cluster[committor_data[:, 0] > 0.01]
+#grid_data = grid_data[committor_data[:, 0] > 0.01]
+#committor_data = committor_data[committor_data[:, 0] > 0.01]
+
 # convert data to batch form
 grid_data = grid_data.reshape(grid_data.shape[0], 1, 64, 64)
-committor_data = committor_data.reshape(committor_data.shape[0], 1)
+committor_data = committor_data.reshape(committor_data.shape[0], 2)
 
 grid_data = torch.from_numpy(grid_data.astype(np.float32))
 committor_data = torch.from_numpy(committor_data.astype(np.float32))
 
 torch.save(grid_data, '/home/eng/phunsc/PhD_Project/2D_Ising_Project/NN/grid_data')
 torch.save(committor_data, '/home/eng/phunsc/PhD_Project/2D_Ising_Project/NN/committor_data')
+
 gridstates.close()
 index_file.close()
+
+plt.scatter(cluster, committor_data[:,0], s=0.5)
+plt.show()
