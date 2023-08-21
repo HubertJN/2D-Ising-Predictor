6cbf5e969fe3875d7620da02b494ae2134b700bd
Hubert Naguszewski <hubert.naguszewski@warwick.ac.uk>
2023-08-21 14:38:31 +0100

    Added plots and made them look better

--- NN/plotting.py
+++ NN/plotting.py
@@ -4,6 +4,7 @@ import torch
 from scipy.optimize import curve_fit
 
 plt.rcParams["font.family"] = "Times New Roman"
+plt.rcParams["font.size"] = 12
 
 prediction_actual = np.load("prediction_actual.npy")
 loss = np.load("loss.npy")
@@ -14,14 +15,14 @@ committor_data = torch.load("./committor_data_tmp")
 committor_data = committor_data.numpy()
 committor_data = committor_data[committor_data[:, 2].argsort()]
 
-std_dev = np.zeros(101)
-comm_percent = 0
+std_dev_net = np.zeros(101)
+mean_net = np.zeros(101)
 for i in range(101):
     index = np.where((np.round(100*prediction_actual[:,0])).astype(np.int32) == i)
-    std_dev[i] = np.std(prediction_actual[index][:,-1])
-    comm_percent += np.sum((abs(prediction_actual[index][:,-1]-prediction_actual[index][:,0])) < std_dev[i])
+    std_dev_net[i] = np.std(prediction_actual[index][:,-1])
+    mean_net[i] = np.mean(prediction_actual[index][:,-1])
 
-comm_percent = comm_percent/len(prediction_actual)*100
+std_dev_net_total = np.std(prediction_actual[:,-1]-prediction_actual[:,0])
 
 # colors
 soft_red = "#ed9688"
@@ -38,49 +39,64 @@ popt, _ = curve_fit(sigmoid, committor_data[:,2], committor_data[:,0], p0=p0)
 
 # committor against cluster size
 plt.scatter(committor_data[:,2], committor_data[:,0], s=1, c=soft_blue, label="Data")
-plt.plot(committor_data[:,2], sigmoid(committor_data[:,2], *popt), c=soft_red, label="Fit")
+plt.plot(committor_data[:,2], sigmoid(committor_data[:,2], *popt), c=soft_red, label="Sigmoid Fit")
 plt.legend()
 plt.title("Test Data Distribution")
 plt.xlabel("Largest Cluster Size")
 plt.ylabel("Committor")
+plt.legend(loc="lower right")
 ax = plt.gca()
 ax.set_box_aspect(1)
 plt.tight_layout()
 plt.show()
 
 # neural network prediction against actual
-plt.scatter(prediction_actual[:,0],prediction_actual[:,-1], s=1, c=soft_red, label="Data")
+#plt.scatter(prediction_actual[:,0],prediction_actual[:,-1], s=1, c=soft_red, label="Data")
 line = np.linspace(0, 1, 101)
-plt.plot(line, line, c=soft_blue, label="Predicted = Actual")
-plt.fill_between(line, line - std_dev, line + std_dev, alpha=0.4, step="mid", color=soft_blue, label="Standard Deviation")
+plt.plot(line, line, c=soft_red, label="Predicted = Actual")
+plt.plot(line, mean_net, c=soft_blue, label="Mean Prediction")
+plt.fill_between(line, mean_net - std_dev_net, mean_net + std_dev_net, alpha=0.2, step="mid", color=soft_blue, label="Prediction Standard Deviation")
 plt.legend(loc="lower right")
 plt.title("Neural Network Prediction Validation")
 plt.xlabel("Actual")
-plt.ylabel("Predicted")
-plt.text(0.01, 0.99, 'Predictions within std dev: {}%'.format(round(comm_percent,1)))
+plt.ylabel("Mean Predicted")
+plt.text(0.01, 0.99, 'Standard Deviation: {}'.format(round(std_dev_net_total,3)))
 ax = plt.gca()
 ax.set_box_aspect(1)
 plt.tight_layout()
 plt.show()
 
 # std dev for fit
+std_dev_fit = np.zeros(101)
+mean_fit = np.zeros(101)
 for i in range(101):
     index = np.where((np.round(100*prediction_actual[:,0])).astype(np.int32) == i)
-    std_dev[i] = np.std(sigmoid(prediction_actual[index][:,2], *popt))
-    comm_percent += np.sum((abs(sigmoid(prediction_actual[index][:,2], *popt)-prediction_actual[index][:,0])) < std_dev[i])
+    std_dev_fit[i] = np.std(sigmoid(prediction_actual[index][:,2], *popt))
+    mean_fit[i] = np.mean(sigmoid(prediction_actual[index][:,2], *popt))
 
-comm_percent = comm_percent/len(prediction_actual)*100
+std_dev_fit_total = np.std(prediction_actual[:,0]-sigmoid(prediction_actual[:,2], *popt))
 
 # max cluster fit prediction against actual
-plt.scatter(prediction_actual[:,0], sigmoid(prediction_actual[:,2], *popt), s=1, c=soft_red, label="Data")
 line = np.linspace(0, 1, 101)
-plt.plot(line, line, c=soft_blue, label="Fit = Actual")
-plt.fill_between(line, line - std_dev, line + std_dev, alpha=0.4, step="mid", color=soft_blue, label="Standard Deviation")
+plt.plot(line, line, c=soft_red, label="Sigmoid Fit = Actual")
+plt.plot(line, mean_fit, c=soft_blue, label="Mean Sigoid Fit")
+plt.fill_between(line, mean_fit - std_dev_fit, mean_fit + std_dev_fit, alpha=0.2, step="mid", color=soft_blue, label="Prediction Standard Deviation")
 plt.legend(loc="lower right")
-plt.title("Fit Prediction Validation")
+plt.title("Sigmoid Fit Validation")
 plt.xlabel("Actual")
-plt.ylabel("Predicted")
-plt.text(0.01, 0.99, 'Predictions within std dev: {}%'.format(round(comm_percent,1)))
+plt.ylabel("Sigmoid Fit")
+plt.text(0.01, 0.99, 'Standard Deviation: {}'.format(round(std_dev_fit_total,3)))
+ax = plt.gca()
+ax.set_box_aspect(1)
+plt.tight_layout()
+plt.show()
+
+# residual of network and fit
+plt.scatter(prediction_actual[:,0]-prediction_actual[:,-1], prediction_actual[:,0]-sigmoid(prediction_actual[:,2], *popt), s=1, c=soft_red, label="Data")
+line = np.linspace(0, 1, 101)
+plt.title("Residual")
+plt.xlabel("Network")
+plt.ylabel("Fit")
 ax = plt.gca()
 ax.set_box_aspect(1)
 plt.tight_layout()
