2580c628c7cf22439e4330521e02453699539fd3
Hubert Naguszewski <hubert.naguszewski@warwick.ac.uk>
2023-10-31 13:45:48 +0000

    Changed to saliency map

--- NN/CAM.py
+++ NN/CAM.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import colors
 from scipy.ndimage import zoom
 from scipy.ndimage import gaussian_filter
 from matplotlib.colors import LinearSegmentedColormap
@@ -15,10 +16,11 @@ plt.rcParams["font.family"] = "Times New Roman"
 plt.rcParams["font.size"] = 18
 plt.rcParams["figure.figsize"] = (8,8)
 plt.rcParams['figure.dpi'] = 120
+plt.rcParams['axes.linewidth'] = 0.5
 
 # plot or evolution?
-plot = True
-evolution = False
+plot = False
+evolution = True
 
 # 1) Model
 
@@ -71,6 +73,15 @@ color_array[:,2] = 0
 map_object = LinearSegmentedColormap.from_list(name='alpha',colors=color_array)
 plt.register_cmap(cmap=map_object)
 
+color_array[:,-1] = np.flip(np.linspace(1.0,0.0, ncolors))
+color_array[:,0] = 0
+color_array[:,1] = 0
+color_array[:,2] = 0
+
+# create a colormap object
+map_object = LinearSegmentedColormap.from_list(name='bw',colors=color_array)
+plt.register_cmap(cmap=map_object)
+
 committor_data = torch.load("./committor_data")
 grid_data = torch.load("./grid_data")
 evolution_committor_data = torch.load("./evolution_committor_data")
@@ -83,7 +94,7 @@ evolution_grid_data = evolution_grid_data.numpy()
 
 # Ising grid
 if plot==True:
-    cluster_choice = 250
+    cluster_choice = 300
     for grid_choice in range(3):
         grid_index = np.where(committor_data[:,-1] == cluster_choice)
         grid_info = grid_data[grid_index][grid_choice,0]
@@ -98,8 +109,9 @@ if plot==True:
         saliency, _ = torch.max(img.grad.data.abs(),dim=1)
         smooth_saliency = gaussian_filter(saliency[0], sigma=1)
 
-        plt.imshow(grid_info, cmap='Greys_r')
-        plt.imshow(smooth_saliency, cmap='alpha')
+        x,y = np.meshgrid(np.arange(grid_info.shape[1]),np.arange(grid_info.shape[0]))
+        plt.pcolor(x,y,smooth_saliency,norm=colors.LogNorm(vmin=smooth_saliency.min(), vmax=smooth_saliency.max()), cmap='coolwarm', edgecolors='face')
+        plt.pcolor(x,y,grid_info, cmap="bw", fc="none", edgecolors=None, lw=0.5)
         plt.title("Base Saliency Map", y=1.01)
         plt.xticks([])
         plt.yticks([])
@@ -118,8 +130,9 @@ if plot==True:
         saliency, _ = torch.max(img.grad.data.abs(),dim=1)
         smooth_saliency = gaussian_filter(saliency[0], sigma=1)
 
-        plt.imshow(grid_info, cmap='Greys_r')
-        plt.imshow(smooth_saliency, cmap='alpha')
+        x,y = np.meshgrid(np.arange(grid_info.shape[1]),np.arange(grid_info.shape[0]))
+        plt.pcolor(x,y,smooth_saliency,norm=colors.LogNorm(vmin=smooth_saliency.min(), vmax=smooth_saliency.max()), cmap='coolwarm', edgecolors='face')
+        plt.pcolor(x,y,grid_info, cmap="bw", fc="none", edgecolors=None, lw=0.5)
         plt.title("LCS Saliency Map", y=1.01)
         plt.xticks([])
         plt.yticks([])
@@ -138,8 +151,9 @@ if plot==True:
         saliency, _ = torch.max(img.grad.data.abs(),dim=1)
         smooth_saliency = gaussian_filter(saliency[0], sigma=1)
 
-        plt.imshow(grid_info, cmap='Greys_r')
-        plt.imshow(smooth_saliency, cmap='alpha')
+        x,y = np.meshgrid(np.arange(grid_info.shape[1]),np.arange(grid_info.shape[0]))
+        plt.pcolor(x,y,smooth_saliency,norm=colors.LogNorm(vmin=smooth_saliency.min(), vmax=smooth_saliency.max()), cmap='coolwarm', edgecolors='face')
+        plt.pcolor(x,y,grid_info, cmap="bw", fc="none", edgecolors=None, lw=0.5)
         plt.title("LCS-P Saliency Map", y=1.01)
         plt.xticks([])
         plt.yticks([])
@@ -180,22 +194,25 @@ if evolution==True:
         smooth_saliency = gaussian_filter(saliency[0], sigma=1)
 
         smooth_saliency_plot[i] = smooth_saliency
+        evolution_committor[i] = [evolution_committor_data[i,0].item(),scores.item()]
 
     grid_info = evolution_grid_data[0][0]
     smooth_saliency = smooth_saliency_plot[0]
-    lattice = ax.imshow(grid_info, cmap='Greys_r', animated=True)
-    heat_map = ax.imshow(smooth_saliency, cmap='alpha', animated=True)
+    x,y = np.meshgrid(np.arange(grid_info.shape[1]),np.arange(grid_info.shape[0]))
+    heat_map = ax.pcolor(x,y,smooth_saliency,norm=colors.LogNorm(vmin=smooth_saliency.min(), vmax=smooth_saliency.max()), cmap='coolwarm', edgecolors='face')
+    lattice = ax.pcolor(x,y,grid_info, cmap="bw", fc="none", edgecolors=None, lw=0.5)
     committor_text = ax.text(0.01, 0.99, 'Committor: {}, Prediction: {}'.format(round(evolution_committor[0,0],4), round(evolution_committor[0,1],4)), transform = ax.transAxes, horizontalalignment='left',
      verticalalignment='top', c='g')
 
     def animate(i):
         i += 1
         grid_info = evolution_grid_data[i][0]
-        smooth_saliency = smooth_saliency_plot[0]
-        lattice.set_data(grid_info)
-        lattice.autoscale()
-        heat_map.set_data(smooth_saliency)
+        smooth_saliency = smooth_saliency_plot[i]
+        heat_map.set_array(smooth_saliency.flatten())
+        heat_map.set_norm(colors.LogNorm(vmin=smooth_saliency.min(), vmax=smooth_saliency.max()))
         heat_map.autoscale()
+        lattice.set_array(grid_info.flatten())
+        lattice.autoscale()
         committor_text.set_text('Committor: {}, Prediction: {}'.format(round(evolution_committor[i,0],4), round(evolution_committor[i,1],4)))
         return lattice, heat_map, committor_text
 
@@ -203,6 +220,6 @@ if evolution==True:
     #writergif = animation.PillowWriter(fps=1) 
     #anim.save('figures/evolution.gif', writer=writergif)
     writer = animation.FFMpegWriter(fps=4)
-    anim.save('figures/evolution.mp4', writer = writer, dpi=240)
+    anim.save('figures/evolution.mp4', writer = writer, dpi=360)
     plt.close()
     #plt.show()
