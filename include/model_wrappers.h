a0628473c12412741fee5ffa54d6f2e4af1629d6
Heather Ratcliffe <h.ratcliffe@warwick.ac.uk>
2024-02-14 17:05:18 +0000

    Fix old commit
    Accidentally committed signature change for launch_mc_sweep, this
    restores correct one

--- include/model_wrappers.h
+++ include/model_wrappers.h
@@ -10,7 +10,7 @@
 
 void init_model(ising_model_config* launch_struct);
 
-void* launch_mc_sweep(void *arg);
+void launch_mc_sweep(cudaStream_t stream, curandState *state, ising_model_config* launch_struct, int *host_array, int *device_array, int stream_ix);
 
 
 
