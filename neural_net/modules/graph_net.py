91298234b6da9f673b09d5e66195b932a22ffc8d
Hubert Naguszewski <hubert.naguszewski@warwick.ac.uk>
2024-04-22 14:48:05 +0100

    cleaned up and commented

--- neural_net/modules/graph_net.py
+++ neural_net/modules/graph_net.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
 
 class graph_net(nn.Module):
@@ -11,23 +10,22 @@ class graph_net(nn.Module):
         self.hidden_n = hidden_n
         self.hidden_m = hidden_m
 
-        self.silu = nn.SiLU()
-        self.relu = nn.ReLU()
+        self.silu = nn.SiLU() # activation function
 
         self.drop = nn.Dropout(0.5)
 
-        self.embed = nn.Linear(1, hidden_n)
+        self.embed = nn.Linear(1, hidden_n) # embedding features into higher dimensional representation
 
-        for i in range(0, k_edge):
+        for i in range(0, k_edge): # loops over k and creates k gcn layers
             self.add_module("gcn_%d" % i, GCNConv(hidden_n, hidden_n))
 
-        self.graph_dec = nn.Sequential(
+        self.graph_dec = nn.Sequential( # graph decoding
             nn.Linear(hidden_n, hidden_m),
             self.drop,
             self.silu,
         )
 
-        self.dec_process = nn.Sequential(
+        self.dec_process = nn.Sequential( # processing decoding
             nn.Linear(hidden_m, hidden_m),
             self.drop,
             self.silu,
@@ -39,73 +37,22 @@ class graph_net(nn.Module):
             self.silu,
         )
 
-        self.output = nn.Sequential(
+        self.output = nn.Sequential( # output layer (alpha and beta of beta distribution)
             nn.Linear(hidden_m, 2),
         )
 
-
-    def forward(self, features, edge_index, n_nodes):
+    def forward(self, features, edge_index, batch_size):
         x = self.embed(features)
 
         # k edge embedding
         for i in range(0, self.k_edge):
             x = self.drop(self.silu(self._modules["gcn_%d" % i](x, edge_index)))
 
-        x = x.view(-1, n_nodes, self.hidden_n)
+        x = x.view(batch_size, -1, self.hidden_n)
         x = torch.mean(x, dim=1)
 
         x = self.graph_dec(x)
         x = self.dec_process(x)
         x = self.output(x)
 
-        return abs(x.squeeze(1))
-
-
-#class graph_net(nn.Module):
-#    def __init__(self, k_edge=2, hidden_n=2):
-#        super(graph_net, self).__init__()
-#
-#        self.k_edge = k_edge
-#        self.hidden_n = hidden_n
-#
-#        self.embed = nn.Linear(1, hidden_n)
-#
-#        self.act_fn = nn.SiLU()
-#
-#        for i in range(0, k_edge):
-#            self.add_module("gcn_%d" % i, GCNConv(hidden_n, hidden_n))
-#
-#        self.node_dec = nn.Sequential(
-#            nn.Linear(hidden_n, hidden_n),
-#            self.act_fn,
-#            nn.Linear(hidden_n, hidden_n),
-#        )
-#
-#        self.graph_dec = nn.Sequential(
-#            nn.Linear(hidden_n, hidden_n),
-#            self.act_fn,
-#            nn.Linear(hidden_n, 1),
-#        )
-#
-#        self.drop = nn.Dropout(p=0.1)
-
-#    def forward(self, features, edge_index, n_nodes):
-#        # coordinate embedding
-#        x = self.embed(features)
-#
-#        # k edge embedding
-#        for i in range(0, self.k_edge):
-#            x = self._modules["gcn_%d" % i](x, edge_index)# + x
-#            x = self.act_fn(x)
-#
-#        # node decoding
-#        x = self.node_dec(x)
-#
-#        # sum pooling
-#        x = x.view(-1, n_nodes, self.hidden_n)
-#        x = torch.sum(x, dim=1) 
-#        
-#        # fully connected layer
-#        x = self.graph_dec(x)
-#
-#        return x.squeeze(1)
+        return abs(x.squeeze(1)) # mapped to absolute value due to alpha,beta > 0
