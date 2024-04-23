f8f187f6df0d7c39ec8ca4641cd2f620bac8e67e
Hubert Naguszewski <hubert.naguszewski@warwick.ac.uk>
2024-04-23 14:25:40 +0100

    rewritten to be more in line with gnn_training script

--- neural_net/nn_train.py
+++ neural_net/nn_train.py
@@ -1,217 +1,92 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import numpy as np
 import sys
 np.set_printoptions(threshold=sys.maxsize)
 np.set_printoptions(linewidth=np.nan)
-import os
-from sklearn import datasets
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-import torchvision
-import torchvision.transforms as transforms
-from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
 
-# selection index for labels training: base, cluster, cluster_perimeter
-selection = "cluster_perimeter"
-# True or False
-checkpoint = True # Use checkpoint system?
-reset = True # Reset if loss to high?
-load = False # Load network?
-run = True # Run training?
-models = 3 # How many models to train
+# 1) loading modules
+##################################################
+# import data loading function
+from modules.cnn_load_data import load_data
 
-# 0) Load data
-# Importing function to load data
-from modules.load_data import load_data
+# import model architecture
+from modules.cnn_architecture import conv_net
 
-# Importing function for accuracy testing
-from modules.test_accuracy import test_accuracy
+# import training function
+from modules.cnn_training import cnn_training
 
-# 1) Model
-from modules.conv_net import conv_net
-    
-# Device configuration and setting up network
+# import weight initialization
+from modules.cnn_weight_initialization import weights_init
+
+# import weight initialization
+from modules.nll_loss_function import loss_func
+
+# 2) setup
+##################################################
+# device configuration
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-device_cpu = torch.device("cpu")
 
-net = conv_net().to(device)
-net_cpu = conv_net().to(device_cpu)  
+# network hyper-parameter choice and initialization
+conv = 3
+channels = 8
+hidden_n = 128
+img_dim = 64
+net = graph_net(conv=conv, channels=channels, hidden_n=hidden_n, img_dim=img_dim).to(device)
+net.apply(weights_init) # initialise weights
 
-# Printing number of parameters
+# training hyper-parameters
+epochs = 5000 # number of training cycles over data
+learning_rate = 5e-4
+weight_decay = 1e-4 # weight parameter for L2 regularization
+train_batch_size = 64
+scheduler_step = 250 # steps before scheduler_gamma is applied to learning rate
+scheduler_gamma = 0.75 # learning rate multiplier every scheduler_step epochs
+
+# loss, optimizer and scheduler
+optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate, weight_decay=weight_decay)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=scheduler_step, gamma=scheduler_gamma)
+
+# loading data
+trainset, valset, testset, train_size, val_size, test_size = load_data()
+train_loader = DataLoader(trainset, shuffle=True, batch_size=train_batch_size)
+val_loader = DataLoader(valset, shuffle=False)
+test_loader = DataLoader(testset, shuffle=False)
+
+# 3) training loop
+##################################################
+# printing number of parameters
 total_params = sum(p.numel() for p in net.parameters())
 print("Parameters: ", total_params)
-#exit()
 
-# 2) Hyper-parameters, loss and optimizer, data loading
-num_epochs = 100
-learning_rate = 1e-3
-weight_decay = 0
-train_batch_size = 100
-test_batch_size = 10
+# running training loop
+net, train_loss, val_loss = cnn_training(epochs, net, device, loss_func, optimizer, scheduler, train_loader, val_loader) 
 
-# Loss and optimizer
-criterion = nn.MSELoss(reduction="mean")
-optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate, weight_decay=weight_decay)
+# 4) saving and plotting data output
+##################################################
+PATH = "./models/cnn_model.pth"
+torch.save({
+    "model_state_dict": net.state_dict(),
+    }, PATH)
 
-# Loading data
-trainset, testset, test_size = load_data(selection=selection)
-trainloader = torch.utils.data.DataLoader(trainset, shuffle=True, num_workers=2, batch_size=train_batch_size)
-testloader = torch.utils.data.DataLoader(testset, shuffle=False, num_workers=2, batch_size=test_batch_size)
+gnn_plot_data = np.zeros([test_size, 3])
+net.eval()
+for i, (features, labels) in enumerate(test_loader):
+        # sending data to device
+        features = features.to(device)
+        labels = labels.to(device)
 
-trainset_subset = torch.utils.data.Subset(trainset, np.random.randint(0, len(trainset), 10000))
-trainloader = torch.utils.data.DataLoader(trainset_subset, shuffle=True, num_workers=2, batch_size=train_batch_size)
+        # performing forward pass
+        predictions = net(features)
 
-# Initialise weights
-def weights_init(m):
-    if isinstance(m, (nn.Linear, nn.Conv2d)):
-        torch.nn.init.xavier_uniform_(m.weight.data)
+        # calculating loss
+        loss = loss_func(labels, predictions[:,0], predictions[:,1])
 
-net.apply(weights_init)
+    plot_data[i,0] = labels.item()
+    plot_data[i,1] = outputs[0,0].item()
+    plot_data[i,2] = outputs[0,1].item()
 
-# 3) Training loop
-
-if selection == "base":
-    PATH = "./models/model_base.pth"
-    loss_path = "./plotting_data/loss_base.npy"
-    prediction_actual_path = "./plotting_data/prediction_actual_base.npy"
-elif selection == "cluster":
-    PATH = "./models/model_lcs.pth"
-    loss_path = "./plotting_data/loss_cluster.npy"
-    prediction_actual_path = "./plotting_data/prediction_actual_cluster.npy"
-elif selection == "cluster_perimeter":
-    PATH = "./models/model_lcs_p.pth"
-    loss_path = "./plotting_data/loss_cluster_perimeter.npy"
-    prediction_actual_path = "./plotting_data/prediction_actual_cluster_perimeter.npy"
-
-# Initializing counters
-accuracy = 0
-revert_to_checkpoint = 0
-revert_limit = 0
-revert_break = 0
-epoch = 0
-min_loss = np.inf
-global_min_loss = np.inf
-lr_power = 0
-num_model = 0
-
-# storage for loss
-loss_arr = np.zeros(num_epochs)
-
-# Loading model
-
-if load == True:
-    checkpoint_model = torch.load(PATH)
-    net.load_state_dict(checkpoint_model["model_state_dict"])
-    optimizer.load_state_dict(checkpoint_model["optimizer_state_dict"])
-    epoch = checkpoint_model["epoch"]
-    accuracy = checkpoint_model["accuracy"]
-    loss_arr = np.load(loss_path)
-    net.eval()
-    print("Loaded NN")
-
-# Running training loop
-
-if run == True:
-    print("Beginning Training Loop")
-    while (1):
-        if epoch >= num_epochs or revert_break >= 4:
-            if (global_min_loss > min_loss):
-                global_min_loss = min_loss
-                np.save(loss_path, loss_arr[:epoch])
-                torch.save({
-                    "epoch": epoch,
-                    "model_state_dict": net.state_dict(),
-                    "optimizer_state_dict": optimizer.state_dict(),
-                    "accuracy": accuracy,
-                    }, PATH)
-            epoch = 0
-            revert_break = 0
-            num_model += 1
-            lr_power = 0
-            if num_model < models:
-                print("Training new model")
-            net.apply(weights_init)
-            optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate, weight_decay=weight_decay)
-            loss_arr = np.zeros(num_epochs)
-            min_loss = np.inf
-
-        if num_model >= models:
-            print("Training finished")
-            print("{} models trained".format(models))
-            print(f"Minimum loss of best model: {global_min_loss:.6f}")
-            break
-
-        epoch += 1
-
-        for i, (images, extra, labels) in enumerate(trainloader):
-            images = images.to(device)
-            extra = extra.to(device)
-
-            # index of labels picks what to train on
-            labels = labels[:,0].to(device)
-            outputs = torch.flatten(net(images, extra))
-            loss = criterion(outputs, labels)
-    
-            # Backward and optimize
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-        revert_to_checkpoint += 1 # Incrementing checkpoint counter
-        test_loss = loss.item()
-        loss_arr[epoch-1] = test_loss
-
-        # Test loss for reset
-        if test_loss > 0.1 and epoch == 5 and reset == True: # Resetting optimizer if accuracy is too low
-            epoch = 1
-            net.apply(weights_init)
-            optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate, weight_decay=weight_decay)
-            print("Network not converging")
-            print("Re-initialising weights and restarting training")
-
-        # Saving checkpoint
-        if (min_loss > test_loss):
-            revert_to_checkpoint = 0
-            revert_limit = 0
-            revert_break = 0
-            min_loss = test_loss
-            torch.save({
-                "epoch": epoch,
-                "model_state_dict": net.state_dict(),
-                "optimizer_state_dict": optimizer.state_dict(),
-                "accuracy": accuracy,
-                }, "./models/training.pth")
-
-        lr = optimizer.param_groups[0]["lr"]
-        print (f"Epoch [{epoch:03d}/{num_epochs}], Loss: {test_loss:.8f}, Minimum Loss: {min_loss:.6f}, Learning Rate: {lr:.10f}")
-        
-        # If no improvement after x epochs, revert to previous checkpoint
-        if revert_to_checkpoint == 10 and checkpoint == True and epoch > 5:
-            revert_to_checkpoint = 0
-            revert_limit += 1
-            revert_break += 1
-            lr_power += 1
-
-            checkpoint_model = torch.load("./models/training.pth")
-            net.load_state_dict(checkpoint_model["model_state_dict"])
-            optimizer.load_state_dict(checkpoint_model["optimizer_state_dict"])
-            epoch = checkpoint_model["epoch"]
-            optimizer.param_groups[0]["lr"] = learning_rate*0.5**lr_power
-            if (revert_break < 4):
-                print("Checkpoint loaded. Starting from epoch: ", epoch)
-
-
-test_batch_size = 1
-outputs_labels = np.zeros([test_size, 7])
-testloader = torch.utils.data.DataLoader(testset, shuffle=False, num_workers=2, batch_size=test_batch_size)
-
-best_model = torch.load(PATH)
-net_cpu.load_state_dict(best_model["model_state_dict"])
-
-net_cpu.eval()
-outputs_labels, _ = test_accuracy(net_cpu, testloader, test_batch_size, outputs_labels)
-
-np.save(prediction_actual_path, outputs_labels)
+np.save("./plotting_data/cnn_prediction_actual.npy", plot_data)
+np.save("./plotting_data/cnn_train_loss.npy", train_loss)
+np.save("./plotting_data/cnn_val_loss.npy", val_loss)
