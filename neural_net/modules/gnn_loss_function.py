c0bc8f0a24267d498088db5e5296b5eb7cc44166
Hubert Naguszewski <hubert.naguszewski@warwick.ac.uk>
2024-04-23 14:06:11 +0100

    renamed to follow cnn and gnn naming scheme

--- neural_net/modules/gnn_loss_function.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-def loss_func(y, alpha, beta): # negative log likelihood (loss function)
-    dist = torch.distributions.beta.Beta(alpha, beta)
-    neg_log_like = torch.mean(torch.clamp(-dist.log_prob(y), max=10))
-    return neg_log_like
