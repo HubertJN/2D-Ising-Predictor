8c7ac73d5ce20c41b17dfe965d67f5cd15118905
Hubert Naguszewski <hubert.naguszewski@warwick.ac.uk>
2024-05-18 12:47:39 +0100

    changed to run over 7 and 8 k_edge

--- neural_net/gnn_batch.sh
+++ neural_net/gnn_batch.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-python gnn_train.py 26 1 5; python gnn_plotting.py
-python gnn_train.py 27 2 5; python gnn_plotting.py
-python gnn_train.py 28 3 5; python gnn_plotting.py
-python gnn_train.py 29 4 5; python gnn_plotting.py
-python gnn_train.py 30 5 5; python gnn_plotting.py
-python gnn_train.py 31 6 5; python gnn_plotting.py
-python gnn_train.py 32 1 6; python gnn_plotting.py
-python gnn_train.py 33 2 6; python gnn_plotting.py
-python gnn_train.py 34 3 6; python gnn_plotting.py
-python gnn_train.py 35 4 6; python gnn_plotting.py
-python gnn_train.py 36 5 6; python gnn_plotting.py
-python gnn_train.py 37 6 6; python gnn_plotting.py
+python gnn_train.py 38 7 1; python gnn_plotting.py
+python gnn_train.py 39 7 2; python gnn_plotting.py
+python gnn_train.py 40 7 3; python gnn_plotting.py
+python gnn_train.py 41 7 4; python gnn_plotting.py
+python gnn_train.py 42 7 5; python gnn_plotting.py
+python gnn_train.py 43 7 6; python gnn_plotting.py
+python gnn_train.py 44 8 1; python gnn_plotting.py
+python gnn_train.py 45 8 2; python gnn_plotting.py
+python gnn_train.py 46 8 3; python gnn_plotting.py
+python gnn_train.py 47 8 4; python gnn_plotting.py
+python gnn_train.py 48 8 5; python gnn_plotting.py
+python gnn_train.py 49 8 6; python gnn_plotting.py
