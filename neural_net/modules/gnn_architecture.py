a9f9dc2c22d28a618b1c2a1a7418d5814c9f4591
Hubert Naguszewski <hubert.naguszewski@warwick.ac.uk>
2024-05-18 10:46:03 +0100

    added slight changes to architecture

--- neural_net/modules/gnn_architecture.py
+++ neural_net/modules/gnn_architecture.py
@@ -14,44 +14,44 @@ class graph_net(nn.Module):
 
         self.drop = nn.Dropout(0.5) # dropout function
 
-        self.embed = nn.Linear(1, self.global_cv) # embedding features into higher dimensional representation
+        self.embed = nn.Sequential( # embedding features into higher dimensional representation
+            nn.Linear(1, self.global_cv),
+        )
 
         for i in range(0, k_edge): # loops over k_edge and creates k_edge gcn layers
             self.add_module("gcn_%d" % i, GCNConv(self.global_cv, self.global_cv))
 
-        self.graph_dec = nn.Sequential( # graph decoding
-            nn.Linear(self.global_cv, hidden_n),
-            self.drop,
-            self.silu,
-        )
-
-        self.dec_process = nn.Sequential( # processing decoding
-            nn.Linear(hidden_n, hidden_n),
-            self.drop,
-            self.silu,
-            nn.Linear(hidden_n, hidden_n),
-            self.drop,
-            self.silu,
-        )
+        for i in range(0, hidden_n): # loops over hidden_n and creates hidden_n fully connected layers
+            self.add_module("fc_%d" % i, nn.Linear(self.global_cv, self.global_cv))
 
         self.output = nn.Sequential( # output layer (alpha and beta of beta distribution)
-            nn.Linear(hidden_n, 2),
+            nn.Linear(self.global_cv, 2),
         )
 
     def forward(self, features, edge_index, batch_size):
         x = self.embed(features)
+        residual = x
 
         # k edge embedding
         for i in range(0, self.k_edge):
-            x = self._modules["gcn_%d" % i](x, edge_index)
             x = self.silu(x)
             x = self.drop(x)
+            x = self._modules["gcn_%d" % i](x, edge_index)
+            x = x + residual
+            residual = x
 
         x = x.view(batch_size, -1, self.global_cv)
         x = torch.mean(x, dim=1)
+        residual = x
 
-        x = self.graph_dec(x)
-        x = self.dec_process(x)
+        # fully connected layers
+        for i in range (0, self.hidden_n):
+            x = self.silu(x)
+            x = self.drop(x)
+            x = self._modules["fc_%d" % i](x)
+            x = x + residual
+            residual = x
+
         x = self.output(x)
 
-        return abs(x.squeeze(1)) # mapped to absolute value due to alpha,beta > 0
+        return abs(x.squeeze(1)) + 0.0001 # mapped to absolute value due to alpha,beta > 0
