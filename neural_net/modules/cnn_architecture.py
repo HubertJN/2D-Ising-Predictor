6084ed9fcf6bbf44d9f2d499d9aa45fe720b73bd
HubertJN <hubert.naguszewski@warwick.ac.uk>
2024-05-18 13:59:42 +0100

    re-written to be in line with gnn architecture, need to test

--- neural_net/modules/cnn_architecture.py
+++ neural_net/modules/cnn_architecture.py
@@ -3,13 +3,13 @@ import torch.nn as nn
 import torchvision.transforms as transforms
 
 class conv_net(nn.Module):
-    def __init__(self, conv=3, channels=2, hidden_n=2, img_dim=64):
+    def __init__(self, conv=1, hidden_n=1, img_dim=64):
         super(conv_net, self).__init__()
 
         self.conv = conv
-        self.channels = channels
         self.hidden_n = hidden_n
         self.img_dim = img_dim
+        self.channels = 8*int((self.conv*(self.conv+1))/2) # coded to be equal to total number of neighbours 
 
         self.resize = transforms.Resize((self.img_dim, self.img_dim), antialias=False) # resize input tensor
 
@@ -19,7 +19,6 @@ class conv_net(nn.Module):
 
         self.pool = nn.MaxPool2d(2,2) # pooling function
 
-                
         self.flatten = torch.flatten(start_dim=1) # flatten function
 
         self.add_module("cn_0", nn.Conv2d(1, channels, 3, padding=[1,1], padding_mode="circular"))
@@ -28,40 +27,42 @@ class conv_net(nn.Module):
 
         conv_output_nodes = (self.img_dim/(2**conv))**2 # input is pooled conv times, each time halving
                                                         # the size of input tensor in both dimensions
-
-        self.conv_decode = nn.Sequential( # convolution decoding
-            nn.Linear(conv_output_nodes, hidden_n),
-            self.drop,
-            self.silu,
         )
 
-        self.fc = nn.Sequential( # fully connected layers
-            nn.Linear(hidden_n, hidden_n),
-            self.drop,
-            self.silu,
-            nn.Linear(hidden_n, hidden_n),
-            self.drop,
-            self.silu,
-        )
+        for i in range(0, hidden_n): # loops over hidden_n and creates hidden_n fully connected layers
+            self.add_module("fc_%d" % i, nn.Linear(conv_output_nodes, conv_output_nodes))
 
         self.output = nn.Sequential( # output layer (alpha and beta of beta distribution)
-            nn.Linear(hidden_n, 2),
+            nn.Linear(conv_output_nodes, 2),
         )
 
     def forward(self, x):
-        x = self.resize(x) # resize input image to 64x64
+        x = self.resize(x) # resize input image to img_dim x img_dim
+        residual = x
 
         # convolutions
         for i in range(0, self.conv):
-            x = self._modules["cn_%d" % i](x)
-            x = self.pool(x)
             x = self.silu(x)
             x = self.drop(x)
+            x = self._modules["cn_%d" % i](x)
+            x = x + residual
+            x = self.pool(x)
+            residual = x
 
         x = self.flatten(x) # flatten
+        residual = x
 
-        x = self.conv_decode(x)
-        x = self.fc(x)
+        # fully connected layers
+        for i in range (0, self.hidden_n):
+            x = self.silu(x)
+            x = self.drop(x)
+            x = self._modules["fc_%d" % i](x)
+            x = x + residual
+            residual = x
+        
+        x = self.silu(x)
+        x = self.drop(x)
         x = self.output(x)
 
-        return abs(x.squeeze(1)) # mapped to absolute value due to alpha,beta > 0
+        return abs(x.squeeze(1)) + 0.0001 # mapped to absolute value due to alpha,beta > 0
+
