ed5e5fda50feed37350e77ca1931d897fe158030
Hubert Naguszewski <hubert.naguszewski@warwick.ac.uk>
2024-05-18 10:46:38 +0100

    added bash file for running training with different hyperparameters

---
+++ neural_net/gnn_batch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+python gnn_train.py 26 1 5; python gnn_plotting.py
+python gnn_train.py 27 2 5; python gnn_plotting.py
+python gnn_train.py 28 3 5; python gnn_plotting.py
+python gnn_train.py 29 4 5; python gnn_plotting.py
+python gnn_train.py 30 5 5; python gnn_plotting.py
+python gnn_train.py 31 6 5; python gnn_plotting.py
+python gnn_train.py 32 1 6; python gnn_plotting.py
+python gnn_train.py 33 2 6; python gnn_plotting.py
+python gnn_train.py 34 3 6; python gnn_plotting.py
+python gnn_train.py 35 4 6; python gnn_plotting.py
+python gnn_train.py 36 5 6; python gnn_plotting.py
+python gnn_train.py 37 6 6; python gnn_plotting.py
