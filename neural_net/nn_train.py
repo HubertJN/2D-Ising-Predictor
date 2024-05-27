e4f6a038e5a19c68a1c359a272081e3f3e189cd5
HubertJN <hubert.naguszewski@warwick.ac.uk>
2024-05-27 18:45:08 +0100

    fixed some syntax bugs after initial rewrite

--- neural_net/nn_train.py
+++ neural_net/nn_train.py
@@ -29,29 +29,35 @@ from modules.nll_loss_function import loss_func
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # network hyper-parameter choice and initialization
-conv = 3
-channels = 8
-hidden_n = 128
+run = 1
+conv = 1
+hidden_n = 1
 img_dim = 64
-net = graph_net(conv=conv, channels=channels, hidden_n=hidden_n, img_dim=img_dim).to(device)
+
+if len(sys.argv) > 1: # if command line arguments provided
+        run = int(sys.argv[1])
+        conv = int(sys.argv[2])
+        hidden_n = int(sys.argv[3])
+
+net = conv_net(conv=conv, hidden_n=hidden_n, img_dim=img_dim).to(device)
 net.apply(weights_init) # initialise weights
 
 # training hyper-parameters
-epochs = 5000 # number of training cycles over data
-learning_rate = 5e-4
+epochs = 1 # number of training cycles over data
+learning_rate = 1e-3
 weight_decay = 1e-4 # weight parameter for L2 regularization
 train_batch_size = 64
-scheduler_step = 250 # steps before scheduler_gamma is applied to learning rate
-scheduler_gamma = 0.75 # learning rate multiplier every scheduler_step epochs
+scheduler_step = 1000 # steps before scheduler_gamma is applied to learning rate
+scheduler_gamma = 0.5 # learning rate multiplier every scheduler_step epochs
 
 # loss, optimizer and scheduler
 optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate, weight_decay=weight_decay)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=scheduler_step, gamma=scheduler_gamma)
 
 # loading data
-trainset, valset, testset, train_size, val_size, test_size = load_data()
+trainset, valset, testset, train_size, val_size, test_size = load_data(device)
 train_loader = DataLoader(trainset, shuffle=True, batch_size=train_batch_size)
-val_loader = DataLoader(valset, shuffle=False)
+val_loader = DataLoader(valset, shuffle=False, batch_size=train_batch_size)
 test_loader = DataLoader(testset, shuffle=False)
 
 # 3) training loop
@@ -74,8 +80,8 @@ gnn_plot_data = np.zeros([test_size, 3])
 net.eval()
 for i, (features, labels) in enumerate(test_loader):
         # sending data to device
-        features = features.to(device)
-        labels = labels.to(device)
+        features = features
+        labels = labels
 
         # performing forward pass
         predictions = net(features)
@@ -83,9 +89,9 @@ for i, (features, labels) in enumerate(test_loader):
         # calculating loss
         loss = loss_func(labels, predictions[:,0], predictions[:,1])
 
-    plot_data[i,0] = labels.item()
-    plot_data[i,1] = outputs[0,0].item()
-    plot_data[i,2] = outputs[0,1].item()
+        plot_data[i,0] = labels.item()
+        plot_data[i,1] = outputs[0,0].item()
+        plot_data[i,2] = outputs[0,1].item()
 
 np.save("./plotting_data/cnn_prediction_actual.npy", plot_data)
 np.save("./plotting_data/cnn_train_loss.npy", train_loss)
