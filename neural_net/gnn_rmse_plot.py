f1d48b75f706b794ddb706998676df8531b62bdf
Hubert Naguszewski <hubert.naguszewski@warwick.ac.uk>
2024-06-05 13:34:26 +0100

    changed axis ticks

--- neural_net/gnn_rmse_plot.py
+++ neural_net/gnn_rmse_plot.py
@@ -14,21 +14,20 @@ for i in range(0, int(np.max(rmse[:,0]))):
     for j in range(0, int(np.max(rmse[:,1]))):
         filtered = rmse[rmse[:,0] == i+1]
         filtered_array = filtered[filtered[:,1] == j+1]
-        #rmse_new[i*int(np.max(rmse[:,1]))+j] = filtered_array[np.argmin(filtered_array[:,2])]
-        rmse_new[i*int(np.max(rmse[:,1]))+j] = np.mean(filtered_array, axis=0)
+        rmse_new[i*int(np.max(rmse[:,1]))+j] = filtered_array[np.argmin(filtered_array[:,2])]
+        #rmse_new[i*int(np.max(rmse[:,1]))+j] = np.mean(filtered_array, axis=0)
 
-print(rmse)
 rmse = rmse_new
 
 x_list = rmse[:,0]
 y_list = rmse[:,1]
 z_list = rmse[:,2]
 
-Z = np.reshape(z_list, [12,12])
+Z = np.reshape(z_list, [int(np.max(rmse[:,0])),int(np.max(rmse[:,1]))])
 Z = Z[::-1]
 
 fig = plt.imshow(Z,
-                extent=[min(x_list),max(x_list),min(y_list),max(y_list)],
+                extent=[min(x_list)-0.5,max(x_list)+0.5,min(y_list)-0.5,max(y_list)+0.5],
                 cmap="viridis_r")
 # Show the positions of the sample points, just to have some reference
 fig.axes.set_autoscale_on(False)
