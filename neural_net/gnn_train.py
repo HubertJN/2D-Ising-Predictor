f0a0385561137e796aa8297be7c35bbd5b128110
Hubert Naguszewski <hubert.naguszewski@warwick.ac.uk>
2024-06-10 13:44:50 +0100

    first version of input arg rewrite. will allow for specifying which network type to load and import approriate modules etc

--- neural_net/gnn_train.py
+++ neural_net/gnn_train.py
@@ -6,54 +6,58 @@ import sys
 np.set_printoptions(threshold=sys.maxsize)
 np.set_printoptions(linewidth=np.nan)
 from torch_geometric.loader import DataLoader
+import importlib
+
+if len(sys.argv) == 5: # if command line arguments provided
+        if int(sys.argv[1]) == 0:
+            net_type = 'cnn'
+        elif int(sys.argv[1]) == 1:
+            net_type = 'gnn'
+        else:
+            net_type = sys.arv[1].lower()
+        run = int(sys.argv[2])
+        k_edge = int(sys.argv[3])
+        hidden_n = int(sys.argv[4])
+else:
+    print('Error. Incorrect input parameters.\nnn_train.py net_type run k_edge hidden_n\nnet_type = "cnn" or "gnn"\nrun, k_edge, hidden_n = int > 0')
+    exit()
 
 # 1) loading modules
 ##################################################
 # import data loading function
-from modules.gnn_load_data import load_data
+load_data = getattr(importlib.import_module('modules.%s_load_data' % net_type), 'load_data')
 
 # import model architecture
-from modules.gnn_architecture import graph_net
+net_init = getattr(importlib.import_module('modules.%s_architecture' % net_type), 'net_init')
 
 # import training function
-from modules.gnn_training import gnn_training
+training = getattr(importlib.import_module('modules.%s_training' % net_type), 'training')
 
 # import weight initialization
-from modules.gnn_weight_initialization import weights_init
+weight_init = getattr(importlib.import_module('modules.%s_weight_init' % net_type), 'weight_init')
 
-# import weight initialization
-from modules.nll_loss_function import loss_func
+# import loss function
+loss_func = getattr(importlib.import_module('modules.nll_loss_function', 'loss_func')
 
 # 2) setup
 ##################################################
 # device configuration
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# network hyper-parameter choice and initialization
-run = 13
-k_edge = 1
-hidden_n = 3
-
-if len(sys.argv) > 1: # if command line arguments provided
-        run = int(sys.argv[1])
-        k_edge = int(sys.argv[2])
-        hidden_n = int(sys.argv[3])
-
-net = graph_net(k_edge=k_edge, hidden_n=hidden_n).to(device)
-net.apply(weights_init) # initialise weights
+net = net_init(k_edge=k_edge, hidden_n=hidden_n).to(device)
+net.apply(weight_init) # initialise weights
 
 try:
-    open("figures/gnn_tweaking/hyperparameters_%d.txt" % run, "r")
+    open("plotting_data/gnn/hyperparameters_%d.txt" % run, "r")
     print("Run already exists. Change run variable. Exiting.")
     exit_status = True
 except:
     exit_status = False
 if exit_status == True:
-    sys.exit()
-
+    """sys.exit()""" ########################## CHANGE LATER ###########################
 
 # training hyper-parameters
-epochs = 3750 # number of training cycles over data
+epochs = 10 # number of training cycles over data
 models = 2 # number of models to train and then select best (lowest loss based on mean of last 50 epochs from validation set)
 learning_rate = 1e-3
 weight_decay = 1e-4 # weight parameter for L2 regularization
@@ -81,16 +85,16 @@ print("Parameters: ", total_params)
 
 
 # running training loop
-gnn_dict = {}
+nn_dict = {}
 for mod in range(models):
-    net, train_loss, val_loss, time_taken = gnn_training(epochs, net, device, loss_func, optimizer, scheduler, train_loader, val_loader) 
+    net, train_loss, val_loss, time_taken = training(epochs, net, device, loss_func, optimizer, scheduler, train_loader, val_loader) 
 
-    gnn_dict["net_%d" % mod] = net
-    gnn_dict["train_loss_%d" % mod] = train_loss
-    gnn_dict["val_loss_%d" % mod] = val_loss
-    gnn_dict["time_taken_%d" % mod] = time_taken
+    nn_dict["net_%d" % mod] = net
+    nn_dict["train_loss_%d" % mod] = train_loss
+    nn_dict["val_loss_%d" % mod] = val_loss
+    nn_dict["time_taken_%d" % mod] = time_taken
     
-    net.apply(weights_init) # initialise weights
+    net.apply(weight_init) # initialise weights
     optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate, weight_decay=weight_decay)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=scheduler_step, gamma=scheduler_gamma)
 
@@ -101,15 +105,14 @@ for mod in range(models):
         min_mod = min_tmp
         mod_choice = mod
 
-net = gnn_dict["net_%d" % mod_choice]
-train_loss = gnn_dict["train_loss_%d" % mod_choice]
-val_loss = gnn_dict["val_loss_%d" % mod_choice]
-time_taken = gnn_dict["time_taken_%d" % mod_choice]
+net = nn_dict["net_%d" % mod_choice]
+train_loss = nn_dict["train_loss_%d" % mod_choice]
+val_loss = nn_dict["val_loss_%d" % mod_choice]
+time_taken = nn_dict["time_taken_%d" % mod_choice]
 
 # 4) saving and plotting data output
 ##################################################
-PATH = "./models/gnn_model_%d.pth" % run
-#net.load_state_dict(torch.load(PATH)['model_state_dict'])
+PATH = "./models/%s_model_%d.pth" % net_type, run
 torch.save({
     "model_state_dict": net.state_dict(),
    }, PATH)
@@ -129,8 +132,15 @@ for i, (batch, index) in enumerate(test_loader):
     plot_data[i,2] = predictions[0,1].item()
     plot_data[i,3] = index.item()
 
+# find rmse of net performance
+alpha = plot_data[:,1]
+beta = plot_data[:,2]
+expectation = alpha/(alpha+beta)
+rmse = np.sqrt(np.mean((plot_data[:,0]-expectation)**2))
+
 # save variables to file
 hyperparameters={'run' : run,
+         'net_type': net_type,
          'k_edge' : k_edge, 
          'hidden_n' : hidden_n,
          'total parameters' : total_params,
@@ -144,12 +154,14 @@ hyperparameters={'run' : run,
          'dataset' : train_size + val_size + test_size,
          'train_size' : train_size,
          'val_size' : val_size,
-         'test_size' : test_size} 
+         'test_size' : test_size,
+         'rmse': rmse
+         }
   
-with open("figures/gnn_tweaking/hyperparameters_%d.txt" % run, 'w') as f:  
+with open("plotting_data/%s/hyperparameters_%d.txt" % net_type, run, 'w') as f:  
     for key, value in hyperparameters.items():  
         f.write('%s = %s\n' % (key, value))
 
-np.save("./plotting_data/gnn_prediction_actual_%d.npy" % run, plot_data)
-np.save("./plotting_data/gnn_train_loss_%d.npy" % run, train_loss)
-np.save("./plotting_data/gnn_val_loss_%d.npy" % run, val_loss)
+np.save("./plotting_data/%s/%s_prediction_actual_%d.npy" % net_type, net_type, run, plot_data)
+np.save("./plotting_data/%s/%s_train_loss_%d.npy" % net_type, net_type, run, train_loss)
+np.save("./plotting_data/%s/%s_val_loss_%d.npy" % net_type, net_type, run, val_loss)
