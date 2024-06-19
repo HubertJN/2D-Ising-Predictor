18d008e4826c60dec58409deaff5fec201a1204a
Hubert Naguszewski <hubert.naguszewski@warwick.ac.uk>
2024-06-19 16:32:43 +0100

    changed file load and file save paths

--- neural_net/gnn_rmse_calc.py
+++ neural_net/gnn_rmse_calc.py
@@ -6,22 +6,29 @@ warnings.filterwarnings("ignore")
 np.set_printoptions(threshold=sys.maxsize)
 np.set_printoptions(linewidth=np.nan)
 
-num_runs = 12*12*2
+num_runs = 144
 rmse = np.zeros([num_runs,3])
 for run in range(1,num_runs+1):
     hyper_dict = {}
-    data = np.load("./plotting_data/gnn_prediction_actual_%d.npy" % run)
-    with open('figures/gnn_tweaking/hyperparameters_%d.txt' % run, 'r') as f:
+    data = np.load("./plotting_data/gnn/gnn_prediction_actual_%d.npy" % run)
+    with open('./plotting_data/gnn/hyperparameters_%d.txt' % run, 'r') as f:
         for line in f:
            (key, val) = line.split(" = ")
            hyper_dict[key] = val.rstrip()
     f.close()
+    
     alpha = data[:,1]
     beta = data[:,2]
     expectation = alpha/(alpha+beta)
     rmse[run-1, 0] = int(hyper_dict["k_edge"])
     rmse[run-1, 1] = int(hyper_dict["hidden_n"])
     rmse[run-1, 2] = np.sqrt(np.mean((data[:,0]-expectation)**2))
+    # TEMPORARY FOR ADDING RMSE TO HYPERPARAMETER FILES
+    hyper_dict['rmse'] = rmse[run-1, 2]
+    with open("./plotting_data/gnn/hyperparameters_%d.txt" % run, 'w') as f:  
+        for key, value in hyperparameters.items():  
+            f.write('%s = %s\n' % (key, value))
+    ##################################################
 
-np.save("figures/gnn_tweaking/rmse.npy", rmse)
+np.save("figures/gnn/rmse.npy", rmse)
 
