6798bcbf318b817baa1e4926c780eebb2c6e64a3
Hubert Naguszewski <hubert.naguszewski@warwick.ac.uk>
2024-06-21 17:21:31 +0100

    removed np.rint functions

--- neural_net/gnn_error_plot.py
+++ neural_net/gnn_error_plot.py
@@ -2,6 +2,8 @@ import numpy as np
 import torch
 import matplotlib.pyplot as plt
 import sys
+np.set_printoptions(threshold=sys.maxsize)
+np.set_printoptions(linewidth=np.nan)
 
 plt.rcParams["font.family"] = "Times New Roman"
 plt.rcParams["font.size"] = 18
@@ -19,13 +21,13 @@ image_data_subset = torch.load("./training_data/image_data_subset").numpy()
 
 num_top = 5
 
-alpha = np.rint(data[:,1])
-beta = np.rint(data[:,2])
+alpha = data[:,1]
+beta = data[:,2]
 expectation = alpha/(alpha+beta)
 error = abs(data[:,0]-expectation)
 top_max = np.argpartition(error, -num_top)[-num_top:]
 grid_index = data[top_max][:,3].astype(np.int32)
-grid_info = image_data_subset[grid_index]
+grid_info = np.squeeze(image_data_subset[grid_index])
 rows, cols = grid_info[0].shape
 cross_size = 0.5
 
@@ -43,7 +45,7 @@ for grid in range(num_top):
                 plt.plot([j + 1 - cross_size/2, j + cross_size/2], [i + cross_size/2, i + 1 - cross_size/2], 'k', linewidth=0.5) 
     
     plt.title("Grid %d" % (grid+1))
-    plt.xlabel("Grid index: %d" % top_max[grid])
+    #plt.xlabel("Grid index: %d" % top_max[grid])
     plt.xlim(0, cols)
     plt.ylim(0, rows)
     plt.gca().set_aspect('equal', adjustable='box')
