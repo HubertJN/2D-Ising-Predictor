cd2663d1fd2bb831886f510ab8fff429100e92fa
Hubert Naguszewski <hubert.naguszewski@warwick.ac.uk>
2024-07-04 19:32:50 +0100

    changed largest error circles to be the same as nn_error_plot ising microstates

--- neural_net/nn_plotting.py
+++ neural_net/nn_plotting.py
@@ -20,8 +20,8 @@ else:
     exit()
 
 plt.rcParams["font.family"] = "Times New Roman"
-plt.rcParams["font.size"] = 18
-plt.rcParams["figure.figsize"] = (8,8)
+plt.rcParams["font.size"] = 25
+plt.rcParams["figure.figsize"] = (12,8)
 plt.rcParams["figure.dpi"] = 120
 
 # colors
@@ -48,19 +48,33 @@ expectation = alpha/(alpha+beta)
 rmse = np.sqrt(np.mean((data[:,0]-expectation)**2))
 variance = np.sqrt(alpha*beta/((alpha+beta)**2*(alpha+beta+1)))
 
-num_top = 5
-error = abs(data[:,0]-expectation)
-top_max = np.argpartition(error, -num_top)[-num_top:]
+num_select = 3
+error = data[:,0]-expectation
+over_index = np.argpartition(error, num_select)[:num_select]
+under_index = np.argpartition(error,- num_select)[-num_select:]
+ideal_index = np.argpartition(abs(error), num_select)[:num_select]
+combined_index = np.concatenate([over_index, ideal_index, under_index])
+
+index_dict = {
+    "over" : over_index,
+    "ideal" : ideal_index,
+    "under" : under_index
+}
 
 plt.plot(line, line, color=soft_red)
 plt.scatter(data[:,0], expectation, s=1, color=soft_blue)
-plt.scatter(data[:,0][top_max], expectation[top_max], s=60, facecolors='none', edgecolors=soft_red)
+plt.scatter(data[:,0][combined_index], expectation[combined_index], s=60, facecolors='none', edgecolors=soft_red)
 plt.xlabel("Target")
 plt.ylabel("Prediction")
 ax = plt.gca()
 
-for i, index in enumerate(top_max):
-    ax.annotate("%d" % (i+1), (data[:,0][index], expectation[index]), (data[:,0][index]-0.02, expectation[index]+0.01), fontsize=10)
+i = 0
+index_type = ["over", "ideal", "under"]
+for index_string in index_type:
+    index_array = index_dict[index_string]
+    for index in index_array:
+        ax.annotate("%d" % (i+1), (data[:,0][index], expectation[index]), (data[:,0][index]-0.02, expectation[index]+0.01), fontsize=10)
+        i += 1
 
 ax.set_box_aspect(1)
 plt.text(0.05, 0.95, "RMSE: {:.5f}".format(rmse), transform = ax.transAxes, horizontalalignment="left",
